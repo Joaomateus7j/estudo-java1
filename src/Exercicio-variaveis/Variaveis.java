e5c531864badd0bbacd19732fe6b998050aacca2
João Mateus <mjoaomateus10@gmail.com>
2026-05-16 16:06:38 -0300

    Teste de commit

---
+++ src/Exercicio-variaveis/Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 25;
+        double salario = 3500.50;
+        String nome = "João";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+    }
+}
