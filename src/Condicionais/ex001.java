481436aa9e6dd3cfcf3b260368186e20181ba89a
João Mateus <mjoaomateus10@gmail.com>
2026-05-16 17:06:58 -0300

    Teste de pastas

--- src/Condicionais/ex001.java
+++ src/Condicionais/ex001.java
@@ -1,10 +1,10 @@
-public class Variaveis {
+package Condicionais;  // ← Adicione isso no topo
+
+public class ex001 {
     public static void main(String[] args) {
         int idade = 25;
         double salario = 3500.50;
-        String nome = "João";
 
-        System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
         System.out.println("Salário: " + salario);
     }
---
+++ src/Variavel/ex002.java
@@ -0,0 +1,5 @@
+package Variavel;
+
+public class ex002 {
+
+}
