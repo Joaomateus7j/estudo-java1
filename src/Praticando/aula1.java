a69b98769e74b16b6f67b987534b2060c3f648a0
João Mateus <mjoaomateus10@gmail.com>
2026-05-17 01:50:40 -0300

    Teste de commit

---
+++ src/Praticando/aula1.java
@@ -0,0 +1,11 @@
+package Praticando;
+
+public class aula1 {
+    public static void main(String[] args) {
+
+        System.out.println("Olá, Mateus!");
+        System.out.println("Estou aprendendo Java 🚀");
+
+    }
+}
+
