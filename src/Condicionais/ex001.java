e69357d37bfc2a8e27fe6147f108f2b653031ca3
João Mateus <mjoaomateus10@gmail.com>
2026-05-16 21:57:53 -0300

    Teste

--- src/Condicionais/ex001.java
+++ src/Condicionais/ex001.java
@@ -1,11 +1,7 @@
-package Condicionais;  // ← Adicione isso no topo
+package Condicionais;  // Se tiver package
 
 public class ex001 {
     public static void main(String[] args) {
-        int idade = 25;
-        double salario = 3500.50;
-
-        System.out.println("Idade: " + idade);
-        System.out.println("Salário: " + salario);
+        // seu código
     }
 }
--- src/Variavel/ex002.java
+++ src/Variavel/ex002.java
@@ -1,5 +1,11 @@
 package Variavel;
 
 public class ex002 {
+    public static void main(String[] args) {
 
+        System.out.println("Olá, Mateus!");
+        System.out.println("Estou aprendendo Java 🚀");
+
+    }
 }
+
