58638771656a7aca814274bc954f070e3f31eac2
João Mateus <mjoaomateus10@gmail.com>
2026-06-03 18:20:46 -0300

    Aula 9- testando comentarios

--- src/academy.devdojo.maratonajava/Main.java
+++ src/academy.devdojo.maratonajava/Main.java
@@ -1,10 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Isso e um comentario
+ */
+
+/**
+ * @param anotação e teste de javadoc
+ */
 public class Main {
     public static void main(String[] args){
         System.out.println("Olá, Mundo");
 
     }
 }
+
+// Isso e um comentario com uma linha
+/*
+   Isso e um comentario de
+   multiplas linhas,
+   estou testando ela agora
+ */
+
