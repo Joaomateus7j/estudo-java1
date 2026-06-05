5a247e883a021f0160f8f7f0145357b96d76aac0
João Mateus <mjoaomateus10@gmail.com>
2026-06-05 19:32:07 -0300

    Estudando String

--- src/main/java/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
+++ src/main/java/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -5,16 +5,19 @@ public class Aula02TiposPrimitivos {
         //int, double, float, char, byte, short, long, boolean
         int idade = 10;
         long numeroGrande = 100000;
-        double salarioDouble = 2000;
+        double salarioDouble = 2500.0D;
         float salarioFloat = 2500;
         byte idadeByte = 121;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 'M';
+
+        String nome = "João mateus dos santos";
         System.out.println("A idade da pessoa e "+idade+" anos");
         System.out.println(verdadeiro);
         System.out.println("char "+ caractere);
+        System.out.println("Oi meu nome e "+nome);
 
     }
 
