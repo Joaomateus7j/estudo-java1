d2d0c78c66eb5e40f061a3eb3d996766c67d6699
João Mateus <mjoaomateus10@gmail.com>
2026-06-06 12:36:38 -0300

    Aplicando exercicios

---
+++ src/main/java/academy/devdojo/maratonajava/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava;
+
+/*
+Prática
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome> morando no endereço <endereço>
+confirmo que recebi o salario de <salario>, na data <data>
+ */
+
+public class Aula03TiposPrimitivosExercicios {
+}
