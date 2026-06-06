f4ccf944788974cae445241b5c4b586afacc522d
João Mateus <mjoaomateus10@gmail.com>
2026-06-06 12:57:39 -0300

    Primeiro exercicio aplicado

--- src/main/java/academy/devdojo/maratonajava/Aula03TiposPrimitivosExercicios.java
+++ src/main/java/academy/devdojo/maratonajava/Aula03TiposPrimitivosExercicios.java
@@ -9,4 +9,16 @@ confirmo que recebi o salario de <salario>, na data <data>
  */
 
 public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+
+        String nome = "João mateus";
+        String endereco = "Rua euflasio, 450";
+        double salario = 2500.50;
+        String data = "06/06/2026";
+
+        System.out.println("Eu " + nome + ", Morando no endereço "+ endereco+ ", Confirmo que recebi o salário de " +salario+ ", Na data de " + data);
+
+
+
+    }
 }
