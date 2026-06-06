44e87ac8f2baed1466340c210b8d91d56d5bc139
João Mateus <mjoaomateus10@gmail.com>
2026-06-06 13:14:36 -0300

    Aplicando

--- src/main/java/academy/devdojo/maratonajava/Aula03TiposPrimitivosExercicios.java
+++ src/main/java/academy/devdojo/maratonajava/Aula03TiposPrimitivosExercicios.java
@@ -11,10 +11,12 @@ confirmo que recebi o salario de <salario>, na data <data>
 public class Aula03TiposPrimitivosExercicios {
     public static void main(String[] args) {
 
+        //--- Variáveis de Texto
         String nome = "João mateus";
         String endereco = "Rua euflasio, 450";
-        double salario = 2500.50;
         String data = "06/06/2026";
+        //Variáveis Numéricas
+        double salario = 2500.50;
 
         System.out.println("Eu " + nome + ", Morando no endereço "+ endereco+ ", Confirmo que recebi o salário de " +salario+ ", Na data de " + data);
 
