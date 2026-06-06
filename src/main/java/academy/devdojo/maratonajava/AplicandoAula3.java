d5d3b68d6f48acc2cf6f4f889f7a390aa88a790f
João Mateus <mjoaomateus10@gmail.com>
2026-06-06 13:42:17 -0300

    Exercicio 3

---
+++ src/main/java/academy/devdojo/maratonajava/AplicandoAula3.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+
+public class AplicandoAula3{
+    public static void main(String[] args) {
+
+        String nomeproduto = "Mouse Gpro ";
+        String modelo = "Logitech ";
+        double preco = 599.90;
+        int quantidade = 5;
+
+        System.out.println("O produto " + nomeproduto + "modelo " + modelo + "está a venda por R$ " + preco + " temos apenas " + quantidade + " unidades em estoque ");
+    }
+}
+
