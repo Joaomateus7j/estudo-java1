80e0479d1b035b2e5ab772e5944c701515f94236
João Mateus <mjoaomateus10@gmail.com>
2026-06-06 14:08:30 -0300

    Exercicio 2 aplicado

---
+++ src/main/java/academy/devdojo/maratonajava/AplicandoAula3Desafio2.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+
+public class AplicandoAula3Desafio2 {
+    public static void main(String[] args) {
+
+        String nome = "João mateus ";
+        int idade = 17;
+        double peso = 77.50;
+        boolean corre = true;
+
+
+        System.out.println("Atleta: " + nome + "| Idade: " + idade + " anos | Peso: " + peso + " kg | Pratica corrida: " + corre);
+    }
+}
