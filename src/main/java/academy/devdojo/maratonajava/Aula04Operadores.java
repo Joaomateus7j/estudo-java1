f042bf7ab3f83236b34dbf783622aa34901a175a
João Mateus <mjoaomateus10@gmail.com>
2026-06-08 15:04:02 -0300

    Testando subtração

--- src/main/java/academy/devdojo/maratonajava/Aula04Operadores.java
+++ src/main/java/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -3,9 +3,9 @@ package academy.devdojo.maratonajava;
 public class Aula04Operadores {
     public static void main(String[] args) {
         // + - / *
-        int numero01 = 10;
-        double numero2 = 20;
-        double resultado = numero2 - numero01;
+        int numero01 = 5;
+        double numero2 = 2;
+        double resultado = numero01 / numero2;
         System.out.println("O valor exibido e de " + resultado);
 
 
