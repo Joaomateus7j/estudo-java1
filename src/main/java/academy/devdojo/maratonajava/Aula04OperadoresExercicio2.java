e628e77540dd07d5b5afd85b2847d16046ed317b
João Mateus <mjoaomateus10@gmail.com>
2026-06-08 18:35:29 -0300

    Praticando mais o (or)

---
+++ src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04OperadoresExercicio2 {
+    public static void main(String[] args) {
+
+        boolean isEstudante = false;
+        int idade = 20;
+
+        boolean isMeiaEntrada = isEstudante || idade < 21;
+        System.out.println("Tem direito a meia-entrada? " + isMeiaEntrada);
+    }
+}
