e5a06b0b5fcc313496af0adf93e48d82c0bc8fbf
João Mateus <mjoaomateus10@gmail.com>
2026-06-08 18:39:47 -0300

    Aplicando

--- src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
+++ src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
@@ -4,7 +4,7 @@ public class Aula04OperadoresExercicio2 {
     public static void main(String[] args) {
 
         boolean isEstudante = false;
-        int idade = 20;
+        int idade = 40;
 
         boolean isMeiaEntrada = isEstudante || idade < 21;
         System.out.println("Tem direito a meia-entrada? " + isMeiaEntrada);
