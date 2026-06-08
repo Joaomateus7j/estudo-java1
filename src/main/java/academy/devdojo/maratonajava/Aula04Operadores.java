9834a015f0ae546bf95231684d933a4e5229034f
João Mateus <mjoaomateus10@gmail.com>
2026-06-08 14:53:31 -0300

    Operadores aritimeticos

---
+++ src/main/java/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero2 = 20;
+        int resultado = numero01 * numero2;
+        System.out.println("O valor exibido e de " + resultado);
+
+
+    }
+}
