56f2e593f6a807d2418c09f5caf382c0911840e7
João Mateus <mjoaomateus10@gmail.com>
2026-06-09 17:10:18 -0300

    Exercicio de Atribuição

--- src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
+++ src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
@@ -8,5 +8,21 @@ public class Aula04OperadoresExercicio2 {
 
         boolean isMeiaEntrada = isEstudante || idade < 21;
         System.out.println("Tem direito a meia-entrada? " + isMeiaEntrada);
+
+
+        // Operadores de atribuição
+        // = += -= *= /= %=
+
+        double bonus = 1800; // 1800 que ele já tava
+        bonus += 2000; // ele somou os 1800 mais os 2000 de bonus e ficou 3800
+        bonus -= 1000; // Ai no segundo bonus ele viu que ta 3800 e subtraiu e deixou 2800
+        bonus *= 2; // Ele vai pegar o * mais o valor que você colocou e vai multiplicar por ele mesmo
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1; // contador = contador + 1;
+        contador++;
+        System.out.println(contador);
     }
 }
