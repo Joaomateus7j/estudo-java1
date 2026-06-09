d5ec3851b39bfea5ff804eecc8c5192aeed067f2
João Mateus <mjoaomateus10@gmail.com>
2026-06-09 19:06:44 -0300

    Aplicando Operadores de atribuição

---
+++ src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio6.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04OperadoresExercicio6 {
+    public static void main(String[] args) {
+
+        // 🧮 Tema 3: Operadores de Atribuição (Exercício 1)
+
+        int pontos = 100;
+        pontos += 50;
+        pontos -= 20;
+
+        System.out.println(pontos);
+
+    }
+}
