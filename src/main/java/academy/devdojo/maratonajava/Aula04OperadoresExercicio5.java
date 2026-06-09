16e42ccfe773d1d3b544a62c43b78e54e9358409
João Mateus <mjoaomateus10@gmail.com>
2026-06-09 18:38:12 -0300

    Aplicando Operadores lógicos

---
+++ src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio5.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04OperadoresExercicio5 {
+    public static void main(String[] args) {
+
+        // 🧠 Tema 2: Operadores Lógicos (Exercício 1)
+
+        boolean isNaLista = true;
+        boolean temConvite = true;
+        boolean isAcessoLiberado = isNaLista && temConvite;
+        System.out.println("isAcessoLiberado? " + isAcessoLiberado);
+    }
+}
