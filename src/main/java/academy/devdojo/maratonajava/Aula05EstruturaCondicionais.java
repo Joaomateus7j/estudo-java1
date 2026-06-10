6ac63cf13d38b6e7cc299abf51e1095005575e7e
João Mateus <mjoaomateus10@gmail.com>
2026-06-10 17:25:44 -0300

    Aplicando primeiro exercicio com Condicional

---
+++ src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturaCondicionais {
+    public static void main(String[] args) {
+
+        int idade = 19;
+        if (idade >= 18){
+            System.out.println("Autorizado a comprar bebida ");
+        }
+        System.out.println("Fora do if");
+
+    }
+}
