409e2319a56cc6bc96601ba7e374260f5ad20d43
João Mateus <mjoaomateus10@gmail.com>
2026-06-10 18:52:41 -0300

    Aplicando else if

--- src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais1.java
+++ src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais1.java
@@ -2,7 +2,16 @@ package academy.devdojo.maratonajava;
 
 public class Aula05EstruturaCondicionais1 {
     public static void main(String[] args) {
-        
-
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade >= 18 categoria adulto
+        int idade = 27;
+        if(idade < 15){
+            System.out.println("Categoria Infantil ");
+        }else if(idade >=15 && idade <18){
+            System.out.println("Categoria Juvenil ");
+        }else {
+            System.out.println("Categoria Adulto ");
+        }
     }
 }
