b84ceadf42f38915a09e5215bbd20fb4b73edeca
João Mateus <mjoaomateus10@gmail.com>
2026-08-10 12:53:52 -0300

    Teste

--- src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
+++ src/main/java/academy/devdojo/maratonajava/Aula04OperadoresExercicio2.java
@@ -16,7 +16,7 @@ public class Aula04OperadoresExercicio2 {
         double bonus = 1800; // 1800 que ele já tava
         bonus += 2000; // ele somou os 1800 mais os 2000 de bonus e ficou 3800
         bonus -= 1000; // Ai no segundo bonus ele viu que ta 3800 e subtraiu e deixou 2800
-        bonus *= 2; // Ele vai pegar o * mais o valor que adaadavocê colocou e vai multiplicar por ele mesmo
+        bonus *= 2; // Ele vai pegar o * mais o valor que você colocou e vai multiplicar por ele mesmo
         System.out.println(bonus);
 
         //
