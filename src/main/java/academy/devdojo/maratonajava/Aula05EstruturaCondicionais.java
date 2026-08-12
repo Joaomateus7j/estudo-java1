34c3b7bffd00943fba8f78764bc7b104ea32f06d
João Mateus <mjoaomateus10@gmail.com>
2026-08-12 11:16:43 -0300

    Teste

--- src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
+++ src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
@@ -2,11 +2,7 @@ package academy.devdojo.maratonajava;
 
 public class Aula05EstruturaCondicionais {
     public static void main(String[] args) {
-
-
-        if(true){
-            System.out.println("Ola ");
-        }
+        
 
     }
 }
