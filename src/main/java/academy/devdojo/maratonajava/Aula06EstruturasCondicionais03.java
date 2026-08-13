2577adae871bebd1393491847fb32bf398741c91
João Mateus <mjoaomateus10@gmail.com>
2026-08-13 18:43:38 -0300

    Aplicando Operador Ternario

--- src/main/java/academy/devdojo/maratonajava/Aula06EstruturasCondicionais03.java
+++ src/main/java/academy/devdojo/maratonajava/Aula06EstruturasCondicionais03.java
@@ -2,18 +2,13 @@ package academy.devdojo.maratonajava;
 
 public class Aula06EstruturasCondicionais03 {
     static void main(String[] args) {
-        double salario = 5000;
+        double salario = 5001;
         String mensagemDoar = "Eu vou dor 500 reais para o Devdojo";
         String mensagemNaoDoar = "Ainda nao tenho condicoes, mas ainda vou ter";
-        String resultado;
+        // (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
 
 
-        if(salario >= 5000) {
-            resultado = mensagemDoar;
-        }else{
-            resultado = mensagemNaoDoar;
-
-        }
         System.out.println(resultado);
 
 
