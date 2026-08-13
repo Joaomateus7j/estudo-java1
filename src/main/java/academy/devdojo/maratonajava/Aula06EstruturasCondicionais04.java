11f8b4be7bc3c06cc631cb93769c025b12781f92
João Mateus <mjoaomateus10@gmail.com>
2026-08-13 19:28:03 -0300

    Aplicando

--- src/main/java/academy/devdojo/maratonajava/Aula06EstruturasCondicionais04.java
+++ src/main/java/academy/devdojo/maratonajava/Aula06EstruturasCondicionais04.java
@@ -2,6 +2,16 @@ package academy.devdojo.maratonajava;
 
 public class Aula06EstruturasCondicionais04 {
     static void main(String[] args) {
-        
+        double salarioAnual = 70000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorImporto;
+
+        if(salarioAnual <= 34712){
+            salarioAnual *= salarioAnual * salarioAnual;
+        } else if (salarioAnual >= 34712 && ) {
+
+        }
     }
 }
