aa415ec0ec270cd479530e56943152151a00f540
João Mateus <mjoaomateus10@gmail.com>
2026-08-13 16:09:21 -0300

    Teste

--- src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
+++ src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
@@ -3,7 +3,6 @@ package academy.devdojo.maratonajava;
 public class Aula05EstruturaCondicionais {
     public static void main(String[] args) {
 
-        int idade = 14;
-        System.out.println(idade);
+        
     }
 }
