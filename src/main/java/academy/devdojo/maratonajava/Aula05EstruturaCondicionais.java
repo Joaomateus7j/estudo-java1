b6350dd59e08307fbacf95be52ec9d5a2827ee24
João Mateus <mjoaomateus10@gmail.com>
2026-08-13 16:42:15 -0300

    Aplicando primeiro if

--- src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
+++ src/main/java/academy/devdojo/maratonajava/Aula05EstruturaCondicionais.java
@@ -3,6 +3,18 @@ package academy.devdojo.maratonajava;
 public class Aula05EstruturaCondicionais {
     public static void main(String[] args) {
 
-        
+        int idade = 17;
+        boolean isAutorizaComprarBebida = idade >= 18;
+
+        // ! 
+
+        if(isAutorizaComprarBebida ){
+            System.out.println("Pode comprar a bebida");
+        }
+        else{
+            System.out.println("Você nao possui idade para comprar bebida");
+        }
+
+
     }
 }
