3b7e8c3a257e84dba68d28455a8feb6c84e0dd07
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 14:06:35 -0300

    Apagando

--- src/main/java/While/Aula09EstruturaDeRepeticao01.java
+++ src/main/java/While/Aula09EstruturaDeRepeticao01.java
@@ -2,9 +2,6 @@ package While;
 
 public class Aula09EstruturaDeRepeticao01 {
     static void main(String[] args) {
-
-        int idade = 12;
-
-        System.out.println(idade);
+        
     }
 }
