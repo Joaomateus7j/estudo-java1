a1c8fe90ac5ab6e39d29728aa4d9a0970c35b705
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 14:36:01 -0300

    Aplicando os 3 conceitos de laço

--- src/main/java/While/Aula09EstruturaDeRepeticao01.java
+++ src/main/java/While/Aula09EstruturaDeRepeticao01.java
@@ -7,9 +7,19 @@ public class Aula09EstruturaDeRepeticao01 {
 
         int count = 0;
         while(count < 10) {
-            System.out.println(count);
+            System.out.println(++count);
 
         }
 
+        do{
+            System.out.println("Dentro do do-while");
+        }while (count < 10);
+
+
+        count = 0;
+        for (int i=0; i < 10; i++){
+            System.out.println("For" +i);
+
+        }
     }
 }
