47ffb235d10d0f6ee1677aa9f192cdbe19e06f50
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 15:04:53 -0300

    Exercicio de For

---
+++ src/main/java/While/Aula09EstruturaDeRepeticao02.java
@@ -0,0 +1,13 @@
+package While;
+
+public class Aula09EstruturaDeRepeticao02 {
+    static void main(String[] args) {
+        // Imprima todos os numeros pares de 0 a 1 milhao
+
+        for (int i = 2; i <= 1000; i++) {
+            System.out.println(i);
+
+
+        }
+    }
+}
