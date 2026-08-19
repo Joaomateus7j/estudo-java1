372cbbe6f20db1fe470a40022f8342d69cf04c16
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 14:11:19 -0300

    Testando while

--- src/main/java/While/Aula09EstruturaDeRepeticao01.java
+++ src/main/java/While/Aula09EstruturaDeRepeticao01.java
@@ -3,5 +3,13 @@ package While;
 public class Aula09EstruturaDeRepeticao01 {
     static void main(String[] args) {
 
+        // while, do while, for
+
+        int count = 0;
+        while(count < 10) {
+            System.out.println(count);
+
+        }
+
     }
 }
