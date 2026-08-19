87b061802ef9ffb34ec602aabe28d5f48b3f1032
João Mateus <mjoaomateus10@gmail.com>
2026-08-18 22:31:46 -0300

    Teste

--- src/main/java/academy/devdojo/maratonajava/Aula06EstruturasCondicionais03.java
+++ src/main/java/academy/devdojo/maratonajava/Aula06EstruturasCondicionais03.java
@@ -4,7 +4,7 @@ public class Aula06EstruturasCondicionais03 {
     static void main(String[] args) {
         double salario = 5001;
         String mensagemDoar = "Eu vou dor 500 reais para o Devdojo";
-        String mensagemNaoDoar = "Ainda nao tenho condicoesd, mas ainda vou ter";
+        String mensagemNaoDoar = "Ainda nao tenho condicoes, mas ainda vou ter";
         // (condicao) ? verdadeiro : falso;
         String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
 
